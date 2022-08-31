f363c516341ff478160cb3d2cff74390402cbeae
schedrov <rocolusso@icloud.com>
2022-08-31 22:39:17 +0300

    added Definitions component to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Definitions from "./Definitions";
+import { definitionsData } from "../utils/definitionsData";
 
 class App extends React.Component {
   render() {
     return (
       <div>
-
+        <Definitions data={definitionsData} />
       </div>
     );
   }
